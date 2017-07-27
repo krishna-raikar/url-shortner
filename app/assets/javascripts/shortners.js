537b79680a63bab04d4680db19c16febb4a639a2
krishna <krishna.raikar@kvpcorp.com>
2017-07-27 18:29:43 +0530

    multiple button click prevented

--- app/assets/javascripts/shortners.js
+++ app/assets/javascripts/shortners.js
@@ -1,3 +1,8 @@
+$("#open_link_btn").unbind('click').bind('click', function(){
+  $("#short_url_form").submit();
+});
+
+
 $("#new_shortner, .edit_shortner").validate({
   rules: {
     "shortner[url]": {

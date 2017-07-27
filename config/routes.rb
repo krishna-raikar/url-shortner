617accd9217b59f112da167c849d6d1e86f3232b
krishna <krishna.raikar@kvpcorp.com>
2017-07-27 18:31:37 +0530

    placeholder for search

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,11 @@ Rails.application.routes.draw do
   root 'shortners#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  #url shortning
+  # url shortning
   post "/short_url" => "shortners#short_url"
 
 
+  # apis
   namespace :api, path: nil, defaults: { format: :json } do
     scope 'v1', module: :v1 do
       post "/shorten_url" => "shortners#create"
